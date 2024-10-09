f419f590fed15da53cae763b14f9a58b2cb0aaef
limagln <gabriellneiva@outlook.com>
2024-10-09 20:01:39 -0300

    First Commit

--- ControleFinanceiro/Form1.cs
+++ ControleFinanceiro/Form1.cs
@@ -38,6 +38,9 @@ namespace ControleFinanceiro
             cliente.Nome = txtNome.Text;
             cliente.Cpf = txtCpf.Text;
             lstClientes.Items.Add(cliente.ToString());
+
+            txtNome.Clear();
+            txtCpf.Clear();
         }
     }
 }
