70bd77d2884280e6688d560ea6b0650ba306ee09
limagln <gabriellneiva@outlook.com>
2024-10-16 20:27:18 -0300

    Add Conta for clients

--- ControleFinanceiro/Model/Cliente.cs
+++ ControleFinanceiro/Model/Cliente.cs
@@ -10,9 +10,23 @@ namespace ControleFinanceiro.Model
     {
         public string Nome {  get;  set; }
         public string Cpf { get; set; }
+        public List<Conta> Conta { get; set; }
+        public Cliente(string nome, string cpf)
+        {
+            Nome = nome;
+            Cpf = cpf;
+        }
         public override string ToString()
         {
             return Nome + " - " + Cpf;
+        }
+        public decimal SaldoDasContas()
+        {
+            decimal total = 0;
+            foreach (Conta conta in Conta) {
+                total += conta.Saldo;
+            }
+            return total;
         }
     }
 }
