c58d390d53453dd8bba92ce688c336c632d8d72c
limagln <gabriellneiva@outlook.com>
2024-10-23 17:27:43 -0300

    Add Transferencia and Conta in Conta

--- ControleFinanceiro/Model/Conta.cs
+++ ControleFinanceiro/Model/Conta.cs
@@ -11,6 +11,17 @@ namespace ControleFinanceiro.Model
         public long Numero { get; set; }
         public decimal Saldo { get; private set; }
         public Cliente Titular { get; set; }
+        public Conta(long numero)
+        {
+            Numero = numero;
+        }
+        public Conta(long numero, decimal saldo, Cliente titular)
+        {
+            Numero = numero;
+            Saldo = saldo;
+            Titular = titular;
+        }
+
         public void Saque (decimal valor)
         {
             if (valor <= Saldo && valor > 0)
@@ -20,6 +31,14 @@ namespace ControleFinanceiro.Model
         {
             if (valor > 0) 
                 Saldo += valor;
+        }
+        public void Transferencia(decimal valor, Conta contaDestino)
+        {
+            if (valor <= Saldo && valor > 0)
+            {
+                Saldo -= valor;
+                contaDestino.Deposito(valor);
+            }
         }
     }
 }
